a9f0dabbe0ed6b408339307ad1371bd0e17667c8
Loïc Juillet <juillet.loic@gmail.com>
2018-06-27 03:49:27 +0200

    Start 0.1

--- Overwatch/MainWindow.xaml.cs
+++ Overwatch/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,22 +24,65 @@ namespace BackgroundApplication
     public partial class MainWindow : Window
     {
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
-        }
+        // UI
+        private double Interval = 1000;
+        private Timer Timer = new Timer();
+
+        // Performance Counters
+        private PerformanceCounter CPU = new PerformanceCounter("Processor", "% Processor Time", "_Total");
 
 
         #region Constructor
 
         public MainWindow()
         {
+            Timer.Interval = Interval;
+            Timer.Elapsed += OnTimerTick;
+            Timer.Start();
+        }
 
+        #endregion
+
+
+
+        private void RefreshValues()
+        {
+            Dispatcher.BeginInvoke((Action)(() => { CPUFieldValue.Text = CPU.NextValue().ToString("00.00") + " %"; }));
+        }
+
+
+
+        #region Callbacks
+
+        /// <summary>
+        /// Called every time Timer has ticked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnTimerTick(object sender, ElapsedEventArgs e)
+        {
+            // Stop Timer so it doesn't run while computing
+            Timer.Stop();
+
+            RefreshValues();
+
+            // Start it again
+            Timer.Start();
+        }
+
+        /// <summary>
+        /// Handle drag action
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
 
         #endregion
 
     }
-    
+
 }
--- Readme.md
+++ Readme.md
@@ -1,3 +1,13 @@
 # Overwatch
 
+
 Simple and lightweight performance report using basic .NET library
+
+
+#### Changelog
+
+
+##### 0.1
+
+
+- Retrieve CPU total usage every seconds
